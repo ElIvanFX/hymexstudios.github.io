f75260c9c12ae5e4d349846e8d2e1775a46fbd95
HymexStudios <154702887+HymexStudios@users.noreply.github.com>
2024-05-28 00:22:57 -0600

    Add files via upload

---
+++ carrito.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Catálogo de Celulares</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+  <main>
+    <div class="producto">
+      <img src="fondo.jpg" alt="Celular 1">
+      <div class="info">
+        <h2>iPhone</h2>
+        <p>Modelo: 15 PRO MAX</p>
+        <p>Precio: $500</p>
+      </div>
+      <div class="botones">
+        <button class="btn-comprar">Comprar</button>
+        <button class="btn-detalles">Ver Detalles</button>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
